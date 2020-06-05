3794755b6c906e1fa736e034df41df2ba50707b2
guanminglin <60331909+guanminglin@users.noreply.github.com>
2020-06-05 15:09:10 -0400

    Add files via upload

---
+++ Monty.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+
+abstract class Monty {
+	public int n_doors, chosen_door, prize;
+
+	Monty(int n, int c, int p) {
+		n_doors = n;
+		chosen_door = c;
+		prize = p;
+	}
+
+	public boolean check_win() {
+		return chosen_door == prize;
+	}
+	
+	public void switch_door(int d) {
+		chosen_door = d;
+	}
+
+	public void execute() {
+		System.out.println("The door you pick is " + chosen_door);
+		int open_door = open_door();
+		if (open_door != 0) {
+			System.out.println("Open door " + open_door);
+			System.out.print("Switch door Y/N ");
+			Scanner scan = new Scanner(System.in);
+			String t = scan.next();
+			if (t.equalsIgnoreCase("Y")) {
+				System.out.print("switch door = ");
+				int temp = scan.nextInt();
+				assert chosen_door <= n_doors;
+				switch_door(temp);
+			}
+		}
+
+		if (check_win()) { System.out.println("WIN"); }
+		else { System.out.println("LOSE");}
+	}
+
+	abstract public int open_door();
+}
+
+
+class RegularMonty extends Monty {
+
+	RegularMonty(int n_doors, int chosen_door, int prize) {
+		super(n_doors, chosen_door, prize);
+	}
+
+	public int open_door() {
+		int result = 0;
+		while (result == 0 || result == chosen_door || result == prize) {
+			Random rand = new Random();
+    		result = rand.nextInt(n_doors) + 1;
+		}
+		return result;
+	}
+}
+
+
+class EvilMonty extends Monty {
+	EvilMonty(int n_doors, int chosen_door, int prize) {
+		super(n_doors, chosen_door, prize);
+	}
+
+	public int open_door() {
+		int result = 0;
+		if (chosen_door == prize) {
+			while (result == 0 || result == chosen_door || result == prize) {
+				Random rand = new Random();
+	    		result = rand.nextInt(n_doors) + 1;
+			}
+		}
+		return result;
+	}
+}
+
+
+class AngelicMonty extends Monty {
+	AngelicMonty(int n_doors, int chosen_door, int prize) {
+		super(n_doors, chosen_door, prize);
+	}
+
+	public int open_door() {
+		int result = 0;
+		if (chosen_door != prize) {
+			while (result == 0 || result == chosen_door || result == prize) {
+				Random rand = new Random();
+	    		result = rand.nextInt(n_doors) + 1;
+			}
+		}
+		return result;
+	}
+}
+
+
---
+++ Monty_Hall.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+
+public class Monty_Hall {
+
+	public static final int REGULAR = 1;
+	public static final int EVIL = 2;
+	public static final int ANGELIC = 3;
+
+
+	public static void main(String[] args) {
+
+		// DEFAULT ARGUMENT VALUES
+
+    	int n_doors = 3;
+    	boolean chosen = false;
+    	int chosen_door = 0;
+    	int monty_type = 0;
+    	boolean debug = false;
+
+
+    	// PARSE ARGUMENTS
+
+    	for(int i = 0; i < args.length; ++i) {
+    		if (args[i].equals("--n_doors")) {
+    			i++;
+    			assert i < args.length;
+    			n_doors = Integer.parseInt(args[i]);
+    		} else if (args[i].equals("--chosen_door")) {
+    			chosen = true;
+    			i++;
+    			assert i < args.length;
+    			chosen_door = Integer.parseInt(args[i]);
+    			assert chosen_door <= n_doors;
+    		} 
+    		else if (args[i].equals("--regular")) { monty_type = REGULAR; }
+    		else if (args[i].equals("--evil")) { monty_type = EVIL; }
+    		else if (args[i].equals("--angelic")) { monty_type = ANGELIC; }
+    		else {
+    			System.out.println("ERROR: UNKNOWN ARGUMENT " + args[i]);
+    			System.exit(1);
+    		}
+    	}	
+
+
+    	// GAME SET-UP
+
+    	Random rand = new Random();
+    	int prize = rand.nextInt(n_doors) + 1;
+
+    	Monty m = new RegularMonty(n_doors, chosen_door, prize);
+    	// if (monty_type == REGULAR) { m = new RegularMonty(n_doors, chosen_door, prize); }
+    	if (monty_type == EVIL) { m = new EvilMonty(n_doors, chosen_door, prize); }
+    	else if (monty_type == ANGELIC) { m = new AngelicMonty(n_doors, chosen_door, prize); }
+
+        m.execute();
+    	System.out.println("prize = " + prize);
+
+
+
+	}
+}
