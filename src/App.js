597c6d27ebc7fa46225d777202b866106b56aa96
Julian <juliancohn@Gmail.com>
2020-06-08 13:43:10 -0400

    change doors text box and fixed mystery mode.

--- src/App.js
+++ src/App.js
@@ -15,50 +15,51 @@ import './App.css';
 
 // top navbar
 class Menu extends React.Component {
-  handleClickHome = (e) => {
-    e.preventDefault();
-    this.props.updateDisplayCB("Home")
-  }
+	handleClickHome = (e) => {
+		e.preventDefault();
+		this.props.updateDisplayCB("Home")
+	}
 
-  handleClickPlay = (e) => {
-    e.preventDefault();
-    this.props.updateDisplayCB("Play");
-  }
+	handleClickPlay = (e) => {
+		e.preventDefault();
+		this.props.updateDisplayCB("Play");
+	}
 
-  handleClickAnswers = (e) => {
-    e.preventDefault();
-    this.props.updateDisplayCB("Answers");
-  }
+	handleClickAnswers = (e) => {
+		e.preventDefault();
+		this.props.updateDisplayCB("Answers");
+	}
 
-  render() {
-    return (
-      <div id="menu" ref="menu">
-        {/* <a id="menuTitle"><img src="../img/logo.png" id="logo"/></a> */}
-        <img src="../img/logo.png" id="logo" onClick={this.handleClickHome}/>
-        <a href ='#' onClick={this.handleClickHome}>Home</a>
-        <a href='#' onClick={this.handleClickAnswers}>Answers</a>
-        <a href='#'>About</a>
-        <a href='#'>Instructions</a>
-        <a href='#' onClick={this.handleClickPlay}>Play</a>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div id="menu" ref="menu">
+			{/* <a id="menuTitle"><img src="../img/logo.png" id="logo"/></a> */}
+			<img src="../img/logo.png" id="logo" onClick={this.handleClickHome}/>
+			<a href ='#' onClick={this.handleClickHome}>Home</a>
+			<a href='#' onClick={this.handleClickAnswers}>Answers</a>
+			<a href='#'>About</a>
+			<a href='#'>Instructions</a>
+			<a href='#' onClick={this.handleClickPlay}>Play</a>
+			</div>
+		);
+	}
 }
 
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "Play",   // display screen
-      mode: "Standard",  // warning screen mode
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: "Play",   // display screen
+			mode: "Standard",  // warning screen mode
 			doors: 3,
 			winning_door: null,
 			door_1: null,
 			m_door: null,
-			door_2: null
-    };
+			door_2: null,
+			mystery: false
+		};
 		this.updateDisplay = (txt) => {this.setState({text:txt})}
 		this.updateMode = (mde) => {this.setState({mode:mde})}
 		this.updateDoors = (d) => {this.setState({doors:d})}
@@ -69,50 +70,52 @@ class App extends React.Component {
 		this.updateMontyDoor = (d) => {this.setState({m_door:d})}
 		this.updateDoor2 = (d) => {this.setState({door_2:d})}
 		this.getMode = () => {return this.state.mode}
-  }
-	
+		this.updateMystery = (b) => {this.setState({mystery:b})}
+	}
 
 
-  // determines which screen takes up main display
-  render() {
+
+	// determines which screen takes up main display
+	render() {
 		var updateDoor;
 		if (this.state.door_1 == null){
 			updateDoor = this.updateDoor1 
 		} else{
 			updateDoor = this.updateDoor2
 		}
-    let display;
+		let display;
 		if (this.state.text == "Home"){
-			display = <Home updateDoors={this.updateDoors}/>
-    } else if (this.state.text === "Play") {
-      display = <Play updateDisplayCB={this.updateDisplay}/>;
-    } else if (this.state.text === "Standard") {
-      display = <StandardPlay monty={this.state.mode} doors={this.state.doors} updateDoor={updateDoor}
-										door_1={this.state.door_1} monty_door={this.state.m_door} mystery={false}/>;
+			display = <Home />
+		} else if (this.state.text === "Play") {
+			display = <Play updateDisplayCB={this.updateDisplay} updateMystery={this.updateMystery} 
+			updateMonty={this.updateMode} updateDoors={this.updateDoors} />;
+		} else if (this.state.text === "Standard") {
+			display = <StandardPlay monty={this.state.mode} doors={this.state.doors} updateDoor={updateDoor}
+			door_1={this.state.door_1} monty_door={this.state.m_door} mystery={this.state.mystery}/>;
 		} else if (this.state.text == "Standard" && this.state.door_1 != null && this.state.door_2 != null) {
 			/* display winning / loosing screen + restart
 			*/
-    } else if (this.state.text === "Answers") {
-      display = <Answers updateDisplayCB={this.updateDisplay}
-                  updateModeCB={this.updateMode}/>;
-    } else if (this.state.text === "Warning") {
-      display = <Warning updateDisplayCB={this.updateDisplay} 
-                  getModeCB={this.getMode}/>
-    } else if (this.state.text === "StandardAnswer") {
-      display = <StdAnswer />;
-    } else if (this.state.text === "HellAnswer") {
-      display = <HellAnswer />
-    } else {
-      display = <Home />;
-    }
+		} else if (this.state.text === "Answers") {
+			display = <Answers updateDisplayCB={this.updateDisplay}
+			updateModeCB={this.updateMode}/>;
+		} else if (this.state.text === "Warning") {
+			display = <Warning updateDisplayCB={this.updateDisplay} 
+			getModeCB={this.getMode}/>
+		} else if (this.state.text === "StandardAnswer") {
+			display = <StdAnswer />;
+		} else if (this.state.text === "HellAnswer") {
+			display = <HellAnswer />
+		} else {
+			display = <Home />;
+		}
 
-    return (
-      <div>
-        <Menu updateDisplayCB={this.updateDisplay}/>,
-        {display}
-      </div>
-    );
-  }
+		return (
+			<div>
+			<Menu updateDisplayCB={this.updateDisplay}/>,
+			{display}
+			</div>
+		);
+	}
 }
 
 
--- src/play.js
+++ src/play.js
@@ -5,38 +5,59 @@ class Play extends React.Component {
         handleClickStd = (e) => {
                 e.preventDefault();
                 this.props.updateDisplayCB("Standard");
+		this.props.updateMonty("Standard");
         }
 
         handleClickHell = (e) => {
                 e.preventDefault();
-                this.props.updateDisplayCB("Hell");
+                this.props.updateDisplayCB("Standard");
+		this.props.updateMonty("Hell");
         }
 
         handleClickAngelic = (e) => {
                 e.preventDefault();
-                this.props.updateDisplayCB("Angelic");
+                this.props.updateDisplayCB("Standard");
+		this.props.updateMonty("Angelic");
         }
 
         handleClickMind = (e) => {
                 e.preventDefault();
-                this.props.updateDisplayCB("Mind");
+                this.props.updateDisplayCB("Standard");
+		this.props.updateMonty("Mind");
         }
 
         handleClickRandom = (e) => {
                 var modes = ["Standard","Hell","Angelic","Mind"];
                 e.preventDefault();
-                this.props.updateDisplayCB(modes[Math.floor(Math.random()*modes.length)]);
+		this.props.updateDisplayCB("Standard");
+                this.props.updateMonty(modes[Math.floor(Math.random()*modes.length)]);
+
+
         }
 
         handleClickMystery = (e) => {
                 e.preventDefault();
-                this.props.updateDisplayCB("Mystery");
+                this.props.updateDisplayCB("Standard");
+		this.props.updateMystery(true);
+                var modes = ["Standard","Hell","Angelic","Mind"];
+                this.props.updateMonty(modes[Math.floor(Math.random()*modes.length)]);
         }
+
+
+	changeDoors = (e) => {
+		console.log(e.target.value)
+		const re = /^[0-9\b]+$/;
+		if (re.test(e.target.value)) {
+			this.props.updateDoors(e.target.value)
+		}
+	}
 
         render () {
                 return (
                         <div className="main1col">
                         <h1>Let's Play</h1>
+			<p> Change number of doors </p>
+			<input value={this.props.doors} onChange={this.changeDoors} />
                         <h2>Select your game mode</h2>
                         <div className="buttons">
                         <div className="rowButton" id="lightBlue" onClick={this.handleClickStd}>
