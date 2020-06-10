30ecea0575069437b1258b546ebb135857637a98
Nikita Bhagam <bha.nikita1@gmail.com>
2020-06-10 10:24:46 -0400

    Add files via upload

--- Monty_Hall.java
+++ Monty_Hall.java
@@ -1,11 +1,15 @@
-import java.util.*;
+//package monty;
 
+import java.util.*;
 
 public class Monty_Hall {
 
 	public static final int REGULAR = 1;
 	public static final int EVIL = 2;
 	public static final int ANGELIC = 3;
+	public static final int PREPARED = 4;
+	public static final int PROBABILISTIC = 5;
+	public static final int RANDOM = 6;
 
 
 	public static void main(String[] args) {
@@ -14,9 +18,10 @@ public class Monty_Hall {
 
     	int n_doors = 3;
     	boolean chosen = false;
-    	int chosen_door = 0;
+    	int chosen_door = 1;
     	int monty_type = 0;
     	boolean debug = false;
+    	boolean monty_type_secret = false; //true if monty type is secret, false otherwise
 
 
     	// PARSE ARGUMENTS
@@ -36,6 +41,9 @@ public class Monty_Hall {
     		else if (args[i].equals("--regular")) { monty_type = REGULAR; }
     		else if (args[i].equals("--evil")) { monty_type = EVIL; }
     		else if (args[i].equals("--angelic")) { monty_type = ANGELIC; }
+    		else if (args[i].equals("--prepared")) {monty_type = PREPARED; }
+    		else if (args[i].equals("--probabilistic")) {monty_type = PROBABILISTIC; }
+    		else if (args[i].equals("--random")) {monty_type = RANDOM; }
     		else {
     			System.out.println("ERROR: UNKNOWN ARGUMENT " + args[i]);
     			System.exit(1);
@@ -45,18 +53,29 @@ public class Monty_Hall {
 
     	// GAME SET-UP
 
-    	Random rand = new Random(System.currentTimeMillis());
+        Random rand = new Random(System.currentTimeMillis());
     	int prize = rand.nextInt(n_doors) + 1;
 
     	Monty m = new RegularMonty(n_doors, chosen_door, prize);
     	// if (monty_type == REGULAR) { m = new RegularMonty(n_doors, chosen_door, prize); }
+    	if(monty_type == RANDOM) 
+    	{ 
+    		monty_type = rand.nextInt(4) + 1;
+    		if(!monty_type_secret)
+    		{
+    			System.out.println("Type of random Monty is " +  monty_type);
+    		}
+    	}
+
     	if (monty_type == EVIL) { m = new EvilMonty(n_doors, chosen_door, prize); }
     	else if (monty_type == ANGELIC) { m = new AngelicMonty(n_doors, chosen_door, prize); }
+    	else if (monty_type == PREPARED) {m = new PreparedMonty(n_doors, chosen_door, prize); }
+    	else if (monty_type == PROBABILISTIC) {m = new ProbabilisticMonty(n_doors, chosen_door, prize); }
 
         m.execute();
-    	System.out.println("prize = " + prize);
+    	System.out.println("Prize was behind the door " + prize);
 
 
 
 	}
-}
+} 
