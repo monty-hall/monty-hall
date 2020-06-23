af2d82596b3e38983698e3e96e60b79de490f436
Julian <juliancohn@Gmail.com>
2020-06-23 12:48:45 -0400

    auto play

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import Play from "./play";
+import React from "react";
 import StandardPlay from "./Standard.js";
-import Home from "./home.js";
+import Home from "./Home.js";
 import Result from "./result.js";
 import Answers from "./answers.js";
 import Warning from "./warning.js";
@@ -11,263 +10,377 @@ import AngelicAnswer from "./angelicAnswer.js";
 import MindAnswer from "./mindAnswer.js";
 import Sandbox from "./sandbox.js";
 
-
-
-import './App.css';
+import "./App.css";
 const axios = require("axios");
 let axiosConfig = {
   headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-      "Access-Control-Allow-Origin": "*",
-  }
+    "Content-Type": "application/json;charset=UTF-8",
+    "Access-Control-Allow-Origin": "*",
+  },
 };
-
 
 // top navbar
 class Menu extends React.Component {
-	handleClickHome = (e) => {
-		e.preventDefault();
-		this.props.updateDisplayCB("Home")
-	}
+  handleClickHome = (e) => {
+    e.preventDefault();
+    this.props.updateDisplayCB("Home");
+  };
 
-	handleClickPlay = (e) => {
-		e.preventDefault();
-		this.props.updateDisplayCB("Standard");
-	}
+  handleClickPlay = (e) => {
+    e.preventDefault();
+    this.props.updateDisplayCB("Standard");
+  };
 
-	handleClickAnswers = (e) => {
-		e.preventDefault();
-		this.props.updateDisplayCB("Answers");
-	}
+  handleClickAnswers = (e) => {
+    e.preventDefault();
+    this.props.updateDisplayCB("Answers");
+  };
 
-	handleClickSandbox = (e) => {
-		e.preventDefault();
-		this.props.updateDisplayCB("Sandbox");
-	}
+  handleClickSandbox = (e) => {
+    e.preventDefault();
+    this.props.updateDisplayCB("Sandbox");
+  };
 
-	render() {
-		return (
-			<div id="menu" ref="menu">
-			{/* <a id="menuTitle"><img src="../img/logo.png" id="logo"/></a> */}
-			<img src="../img/logo.png" id="logo" onClick={this.handleClickHome}/>
-			<a href ='#' onClick={this.handleClickHome}>Home</a>
-			<a href='#' onClick={this.handleClickAnswers}>Answers</a>
-			<a href='#'>About</a>
-			<a href='#'>Instructions</a>
-			<a href='#' onClick={this.props.logOff}> End Session </a>
-			{this.props.sandbox &&
-				<a href='#' onClick={this.handleClickSandbox}>Sandbox</a>
-			}
-			<a href='#' onClick={this.handleClickPlay}>Play</a>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="menu" ref="menu">
+        {/* <a id="menuTitle"><img src="../img/logo.png" id="logo"/></a> */}
+        <img
+          src="../img/logo.png"
+          id="logo"
+          onClick={this.handleClickHome}
+          alt={"logo"}
+        />
+        <a href="#" onClick={this.handleClickHome}>
+          Home
+        </a>
+        <a href="#" onClick={this.handleClickAnswers}>
+          Answers
+        </a>
+        <a href="#">About</a>
+        <a href="#">Instructions</a>
+        <a href="#" onClick={this.props.logOff}>
+          {" "}
+          End Session{" "}
+        </a>
+        {this.props.sandbox && (
+          <a href="#" onClick={this.handleClickSandbox}>
+            Sandbox
+          </a>
+        )}
+        <a href="#" onClick={this.handleClickPlay}>
+          Play
+        </a>
+      </div>
+    );
+  }
 }
-
-
 
 class App extends React.Component {
-	/*
-	 * TODO: move constructor functions to class functions
-	 * make session creation add cookie
-	 * check for cookie and if it does not exist put in default mode with random monty
-	 */
-	constructor(props) {
-		super(props);
-		this.state = {
-			session: null, // int unique to each user. Saved to local storge 
-			sandboxAvailable: false, // Has the user played the preliminary round untill finished?
-			text: "Home",   // display screen
-			mode: "Standard",  // what monty are we using?
-			doors: 3,
-			winning_door: null, 
-			door_1: null,	// first door selected
-			m_door: null,	// door Monty reveals
-			door_2: null,	// second door selected (on switch)
-			mystery: true // Do we tell the user what monty they are using
-		};
-	}
-	logOff = () => {
-		// call backend to ask for statistics
-		// display statistics
-		// resets state for next user
-		this.setState({
-			session: null, 
-			sandboxAvailable: false,
-			text: "Stats",
-			mode: "Standard",
-			doors: 3,
-			winning_door: null, 
-			door_1: null,	
-			m_door: null,	
-			door_2: null,	
-			mystery: true 
-		});
-		window.localStorage.clear();
-		this.getSession();
-	}
-	getSession = () => {
-		axios.get("http://127.0.0.1:5000/new_session")
-		.then((response) => {
-			window.localStorage.setItem('user', JSON.stringify({
-				session: response.data.session_id, sandboxAvailable: false
-			}));
-			this.setState({session:response.data.session_id});
-			} ,(error) => {console.log(error);}
-		);
-	};
-	updateMontyDoorAPI = async function(d) {
-		axios.post("http://127.0.0.1:5000/game", {
-			monty: this.state.mode,
-			doors: this.state.doors,
-			winning_door: this.state.winning_door,
-			door_1: d
-		} , axiosConfig)
-		.then((response) => {
-			this.setState({m_door: response.data.monty_door});
-		} ,(error) => {console.log(error);}
-		);
-	};
-	addToDb = (d) => {
-		axios.post("http://127.0.0.1:5000/end", {
-			sess: this.state.session,
-			monty: this.state.mode,
-			doors: this.state.doors,
-			winning_door: this.state.winning_door,
-			door_1: this.state.door_1,
-			m_door: this.state.m_door,
-			door_2: d
-		});
-	};
-	updateDisplay = (txt) => {this.setState({text:txt})};
-	updateMode = (mde) => {this.setState({mode:mde})};
-	updateDoors = (d) => {this.setState({doors:d})};
-	updateDoor1 = (d) => {
-		this.setState({door_1:d});
-		this.updateMontyDoorAPI(d);
-		/*do api call asyncronously using axios -> update m_door*/
-	};
-	updateMontyDoor = (d) => {this.setState({m_door:d})}
-	updateDoor2 = (d) => {
-		this.setState({door_2:d});
-		this.addToDb(d);
-	};
-	getMode = () => {return this.state.mode};
-	updateMystery = (b) => {this.setState({mystery:b})};
-	updateWinner = (d) => {
-		this.setState({
-			winning_door: Math.floor(Math.random() * this.state.doors),
-			door_1: null,
-			m_door: null,
-			door_2: null
-		})
-	};
-	resetDoors = () => {this.setState({door_1: null, door_2: null, m_door: null, winning_door: null})};
-	toSandboxMode = () => {
-		this.setState({
-			text: "Sandbox", 
-			monty: null,
-			door_1: null,
-			door_2: null,
-			m_door: null,
-			winning_door: null,
-			sandboxAvailable:true
-		})
-		window.localStorage.setItem('user', JSON.stringify({session: this.state.session, sandboxAvailable: true}));
-	};
-	toSandboxScreen = () => {
-		this.setState({
-			text: "Sandbox", 
-			monty: null,
-			door_1: null,
-			door_2: null,
-			m_door: null,
-			winning_door: null,
-			sandboxAvailable:true
-		})
-	};
+  /*
+   * TODO: move constructor functions to class functions
+   * make session creation add cookie
+   * check for cookie and if it does not exist put in default mode with random monty
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      session: null, // int unique to each user. Saved to local storge
+      sandboxAvailable: false, // Has the user played the preliminary round untill finished?
+      text: "Home", // display screen
+      mode: "Standard", // what monty are we using?
+      doors: 3,
+      winning_door: null,
+      door_1: null, // first door selected
+      m_door: null, // door Monty reveals
+      door_2: null, // second door selected (on switch)
+      mystery: true, // Do we tell the user what monty they are using
+    };
+  }
+  logOff = () => {
+    // call backend to ask for statistics
+    // display statistics
+    // resets state for next user
+    this.setState({
+      session: null,
+      sandboxAvailable: false,
+      text: "Stats",
+      mode: "Standard",
+      doors: 3,
+      winning_door: null,
+      door_1: null,
+      m_door: null,
+      door_2: null,
+      mystery: true,
+    });
+    window.localStorage.clear();
+    this.getSession();
+  };
+  getSession = () => {
+    axios.get("http://127.0.0.1:5000/new_session").then(
+      (response) => {
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            session: response.data.session_id,
+            sandboxAvailable: false,
+          })
+        );
+        this.setState({ session: response.data.session_id });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+  updateMontyDoorAPI = async function (d) {
+    axios
+      .post(
+        "http://127.0.0.1:5000/game",
+        {
+          monty: this.state.mode,
+          doors: this.state.doors,
+          winning_door: this.state.winning_door,
+          door_1: d,
+        },
+        axiosConfig
+      )
+      .then(
+        (response) => {
+          this.setState({ m_door: response.data.monty_door });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+  addToDb = (d) => {
+    axios.post("http://127.0.0.1:5000/end", {
+      sess: this.state.session,
+      monty: this.state.mode,
+      doors: this.state.doors,
+      winning_door: this.state.winning_door,
+      door_1: this.state.door_1,
+      m_door: this.state.m_door,
+      door_2: d,
+    });
+  };
+  updateDisplay = (txt) => {
+    this.setState({ text: txt });
+  };
+  updateMode = (mde) => {
+    this.setState({ mode: mde });
+  };
+  updateDoors = (d) => {
+    this.setState({ doors: d });
+  };
+  updateDoor1 = (d) => {
+    this.setState({ door_1: d });
+    this.updateMontyDoorAPI(d);
+    /*do api call asyncronously using axios -> update m_door*/
+  };
+  updateMontyDoor = (d) => {
+    this.setState({ m_door: d });
+  };
+  updateDoor2 = (d) => {
+    this.setState({ door_2: d });
+    this.addToDb(d);
+  };
+  getMode = () => {
+    return this.state.mode;
+  };
+  updateMystery = (b) => {
+    this.setState({ mystery: b });
+  };
+  updateWinner = (d) => {
+    this.setState({
+      winning_door: Math.floor(Math.random() * this.state.doors),
+      door_1: null,
+      m_door: null,
+      door_2: null,
+    });
+  };
+  resetDoors = () => {
+    this.setState({
+      door_1: null,
+      door_2: null,
+      m_door: null,
+      winning_door: null,
+    });
+  };
+  toSandboxMode = () => {
+    this.setState({
+      text: "Sandbox",
+      monty: null,
+      door_1: null,
+      door_2: null,
+      m_door: null,
+      winning_door: null,
+      sandboxAvailable: true,
+    });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({ session: this.state.session, sandboxAvailable: true })
+    );
+  };
+  toSandboxScreen = () => {
+    this.setState({
+      text: "Sandbox",
+      monty: null,
+      door_1: null,
+      door_2: null,
+      m_door: null,
+      winning_door: null,
+      sandboxAvailable: true,
+    });
+  };
+  autoPlay = (count) => {
+    axios.post("http://127.0.0.1:5000/auto", {
+      sess: this.state.session,
+      monty: this.state.mode,
+      doors: this.state.doors,
+      count: count,
+    });
+  };
 
+  // determines which screen takes up main display
+  render() {
+    var updateDoor;
+    // IF session id is null, check local storage. If both are null, create new session
+    if (this.state.session == null) {
+      const res = window.localStorage.getItem("user");
+      if (res == null) {
+        this.getSession();
+      } else {
+        const stored = JSON.parse(res);
+        if (stored == null || stored.session == null) {
+          this.getSession();
+        } else {
+          this.setState({
+            session: stored.session,
+            sandboxAvailable: stored.sandboxAvailable,
+          });
+        }
+      }
+    }
+    if (
+      (this.state.sandboxAvailable === false ||
+        this.state.text === "Standard") &&
+      this.state.mode == null
+    ) {
+      var modes = ["Standard", "Hell", "Angelic", "Mind"];
+      var monty = modes[Math.floor(Math.random() * modes.length)];
+      this.setState({ mode: monty });
+    }
+    if (this.state.door_1 == null) {
+      updateDoor = this.updateDoor1;
+    } else {
+      updateDoor = this.updateDoor2;
+    }
+    let display;
+    if (
+      this.state.sandboxAvailable === false &&
+      this.state.door_1 != null &&
+      this.state.door_2 != null
+    ) {
+      display = (
+        <Result
+          selected={this.state.door_2}
+          correct={this.state.winning_door}
+          resetDoors={this.resetDoors}
+          toPlayScreen={this.toSandboxMode}
+          t={"Finish and play sandbox mode"}
+        />
+      );
+    } else if (this.state.sandboxAvailable === false) {
+      display = (
+        <StandardPlay
+          monty={this.state.mode}
+          doors={this.state.doors}
+          updateDoor={updateDoor}
+          door_1={this.state.door_1}
+          monty_door={this.state.m_door}
+          mystery={this.state.mystery}
+        />
+      );
+    } else if (this.state.text === "Sandbox") {
+      display = (
+        <Sandbox
+          updateDisplayCB={this.updateDisplay}
+          updateMystery={this.updateMystery}
+          updateMonty={this.updateMode}
+          updateDoors={this.updateDoors}
+          updateWinningDoor={this.updateWinner}
+        />
+      );
+    } else if (
+      this.state.text === "Standard" &&
+      this.state.door_1 != null &&
+      this.state.door_2 != null
+    ) {
+      // display winning / loosing screen + restart
+      display = (
+        <Result
+          selected={this.state.door_2}
+          correct={this.state.winning_door}
+          resetDoors={this.resetDoors}
+          toPlayScreen={this.toSandboxScreen}
+          t={"New Game"}
+        />
+      );
+    } else if (this.state.text === "Standard") {
+      display = (
+        <StandardPlay
+          monty={this.state.mode}
+          doors={this.state.doors}
+          updateDoor={updateDoor}
+          door_1={this.state.door_1}
+          monty_door={this.state.m_door}
+          mystery={this.state.mystery}
+        />
+      );
+    } else if (this.state.text === "Answers") {
+      display = (
+        <Answers
+          updateDisplayCB={this.updateDisplay}
+          updateModeCB={this.updateMode}
+        />
+      );
+    } else if (this.state.text === "Warning") {
+      display = (
+        <Warning
+          updateDisplayCB={this.updateDisplay}
+          getModeCB={this.getMode}
+        />
+      );
+    } else if (this.state.text === "StandardAnswer") {
+      display = <StdAnswer />;
+    } else if (this.state.text === "HellAnswer") {
+      display = <HellAnswer />;
+    } else if (this.state.text === "AngelicAnswer") {
+      display = <AngelicAnswer />;
+    } else if (this.state.text === "MindAnswer") {
+      display = <MindAnswer />;
+    } else if (this.state.text === "Stats") {
+      // DISPLAY STATISTICS
+      display = null;
+    } else if (this.state.text === "Home") {
+      display = <Home />;
+    } else {
+      display = <Home />;
+    }
 
-
-	// determines which screen takes up main display
-	render() {
-		var updateDoor;
-		// IF session id is null, check local storage. If both are null, create new session
-		if (this.state.session == null) {
-			const res = window.localStorage.getItem('user');
-			if (res == null){
-				this.getSession()
-			} else {
-				const stored = JSON.parse(res);
-				if (stored == null || stored.session == null) {
-					this.getSession()
-				} else {
-					this.setState({session: stored.session, sandboxAvailable: stored.sandboxAvailable});
-				}
-			}
-		}
-		if ((this.state.sandboxAvailable === false || this.state.text === "Standard") && this.state.mode == null){
-			var modes = ["Standard","Hell","Angelic","Mind"];
-			var monty = modes[Math.floor(Math.random()*modes.length)];
-			this.setState({mode:monty});
-		}
-		if (this.state.door_1 == null){
-			updateDoor = this.updateDoor1 
-		} else{
-			updateDoor = this.updateDoor2
-		}
-		let display;
-		if (this.state.sandboxAvailable === false && this.state.door_1 != null && this.state.door_2 != null){
-			display = <Result selected={this.state.door_2} correct={this.state.winning_door}
-			resetDoors={this.resetDoors} toPlayScreen={this.toSandboxMode} t={"Finish and play sandbox mode"}/>
-		} else if (this.state.sandboxAvailable === false){
-			display = <StandardPlay monty={this.state.mode} doors={this.state.doors} updateDoor={updateDoor}
-			door_1={this.state.door_1} monty_door={this.state.m_door} mystery={this.state.mystery}/>;
-		} else if (this.state.text === "Play") {
-			display = <Play updateDisplayCB={this.updateDisplay} updateMystery={this.updateMystery} 
-			updateMonty={this.updateMode} updateDoors={this.updateDoors} updateWinningDoor={this.updateWinner}/>;
-		} else if (this.state.text === "Sandbox") {
-			display = <Sandbox updateDisplayCB={this.updateDisplay} updateMystery={this.updateMystery} 
-			updateMonty={this.updateMode} updateDoors={this.updateDoors} updateWinningDoor={this.updateWinner}/>;
-		} else if (this.state.text === "Standard" && this.state.door_1 != null && this.state.door_2 != null) {
-			// display winning / loosing screen + restart
-			display = <Result selected={this.state.door_2} correct={this.state.winning_door} 
-			resetDoors={this.resetDoors} toPlayScreen={this.toSandboxScreen} t={"New Game"}/>
-		} else if (this.state.text === "Standard") {
-			display = <StandardPlay monty={this.state.mode} doors={this.state.doors} updateDoor={updateDoor}
-			door_1={this.state.door_1} monty_door={this.state.m_door} mystery={this.state.mystery}/>;
-		}	else if (this.state.text === "Answers") {
-			display = <Answers updateDisplayCB={this.updateDisplay}
-			updateModeCB={this.updateMode}/>;
-		} else if (this.state.text === "Warning") {
-			display = <Warning updateDisplayCB={this.updateDisplay} 
-			getModeCB={this.getMode}/>
-		} else if (this.state.text === "StandardAnswer") {
-			display = <StdAnswer />;
-		} else if (this.state.text === "HellAnswer") {
-			display = <HellAnswer />;
-		} else if (this.state.text === "AngelicAnswer") {
-			display = <AngelicAnswer />;
-	 	} else if (this.state.text === "MindAnswer") {
-	 		display = <MindAnswer />;
-	 	} else if (this.state.text === "Stats") {
-			// DISPLAY STATISTICS
-			display = null;
-		} else if (this.state.text === "Home") {
-			display = <Home />;
-		} else {
-			display = <Home />;
-		}
-
-		return (
-			<div>
-			<Menu updateDisplayCB={this.updateDisplay} sandbox={this.state.sandboxAvailable} 
-			logOff={this.logOff}/>,
-			{display}
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Menu
+          updateDisplayCB={this.updateDisplay}
+          sandbox={this.state.sandboxAvailable}
+          logOff={this.logOff}
+        />
+        ,{display}
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- src/sandbox.js
+++ src/sandbox.js
@@ -2,110 +2,179 @@ import React from "react";
 import "./App.css";
 
 class Sandbox extends React.Component {
-	changeDisplayAndMonty = (m) => {
-		this.props.updateMonty(m);
-		this.props.updateWinningDoor();
-		this.props.updateDisplayCB("Standard");
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: false,
+      autoPlay: false,
+      auto_count: 10,
+    };
+  }
 
-	handleClickStd = (e) => {
-		e.preventDefault();
-		this.changeDisplayAndMonty("Standard");
-	}
-
-    handleClickHell = (e) => {
-    	e.preventDefault();
-		this.changeDisplayAndMonty("Hell");
+  changeDisplay = (m) => {
+    if (this.state.autoPlay) {
+      this.props.autoPlay(this.state.auto_count);
+    } else {
+      this.props.updateWinningDoor();
+      this.props.updateDisplayCB("Standard");
     }
+  };
 
-    handleClickAngelic = (e) => {
-    	e.preventDefault();
-		this.changeDisplayAndMonty("Angelic");
-	}
+  handleClickStd = (e) => {
+    e.preventDefault();
+    this.props.updateMonty("Standard");
+  };
 
-    handleClickMind = (e) => {
-    	e.preventDefault();
-        this.props.updateDisplayCB("Standard");
-		this.changeDisplayAndMonty("Mind");
-	}
+  handleClickHell = (e) => {
+    e.preventDefault();
+    this.props.updateMonty("Hell");
+  };
 
-    handleClickRandom = (e) => {
-    	var modes = ["Standard","Hell","Angelic","Mind"];
-        e.preventDefault();
-        var monty = modes[Math.floor(Math.random()*modes.length)];
-		this.changeDisplayAndMonty(monty);
-	}
+  handleClickAngelic = (e) => {
+    e.preventDefault();
+    this.props.updateMonty("Angelic");
+  };
 
-    handleClickMystery = (e) => {
-    	e.preventDefault();
-		this.props.updateMystery(true);
-        var modes = ["Standard","Hell","Angelic","Mind"];
-        var monty = modes[Math.floor(Math.random()*modes.length)];
-		this.changeDisplayAndMonty(monty);
+  handleClickMind = (e) => {
+    e.preventDefault();
+    this.props.updateDisplayCB("Standard");
+    this.props.updateMonty("Mind");
+  };
+
+  handleClickRandom = (e) => {
+    var modes = ["Standard", "Hell", "Angelic", "Mind"];
+    e.preventDefault();
+    var monty = modes[Math.floor(Math.random() * modes.length)];
+    this.props.updateMonty(monty);
+  };
+
+  handleClickMystery = (e) => {
+    e.preventDefault();
+    this.props.updateMystery(true);
+    var modes = ["Standard", "Hell", "Angelic", "Mind"];
+    var monty = modes[Math.floor(Math.random() * modes.length)];
+    this.props.updateMonty(monty);
+  };
+
+  changeDoors = (e) => {
+    console.log(e.target.value);
+    const re = /^[0-9\b]+$/;
+    if (re.test(e.target.value)) {
+      this.props.updateDoors(parseInt(e.target.value));
     }
+    this.props.updateWinningDoor();
+  };
 
-    changeDoors = (e) => {
-		console.log(e.target.value)
-		const re = /^[0-9\b]+$/;
-		if (re.test(e.target.value)) {
-			this.props.updateDoors(parseInt(e.target.value));
-		}
-		this.props.updateWinningDoor();
-	}
-
-	render() {
-		return(
-			<div className="main1col">
-                <h1>Sandbox Mode</h1>
-                <div className="inlineDiv">
-					<input id="numDoors" value={this.props.doors} onChange={this.changeDoors} />
-					<label for="numDoors">Number of Doors</label>
-				</div>
-                <h2>Select your game mode</h2>
-                <div className="buttons">
-                   	<div className="rowButton" id="lightBlue" onClick={this.handleClickStd}>
-                       	<span>Standard Monty</span>
-                    </div>
-                    <div className="rowButton" id="red" onClick={this.handleClickHell}>
-                       	<span>Hell Monty</span>
-					</div>
-                    <div className="rowButton" id="gold" onClick={this.handleClickAngelic}>
-                    	<span>Angelic Monty</span>
-                    </div>
-                    <div className="rowButton" id="violet" onClick={this.handleClickMind}>
-                    	<span>Mind-Reading Monty</span>
-                    </div>
-                    <div className="rowButton" id="skyBlue" onClick={this.handleClickRandom}>
-                    	<span>Random Monty</span>
-                    </div>
-                    <div className="rowButton" id="navy" onClick={this.handleClickMystery}>
-                    	<span>Mystery Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-                    <div className="rowButton" >
-                    	<span>??? Monty</span>
-                    </div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    var options_button;
+    if (this.state.options) {
+      options_button = (
+        <button onClick={() => this.setState({ options: false })}>
+          {" "}
+          LESS OPTIONS{" "}
+        </button>
+      );
+    } else {
+      options_button = (
+        <button onClick={() => this.setState({ options: true })}>
+          {" "}
+          MORE OPTIONS{" "}
+        </button>
+      );
+    }
+    return (
+      <div className="main1col">
+        <h1>Sandbox Mode</h1>
+        <button onClick={this.changeDisplay}> PLAY</button>
+        <div className="inlineDiv">
+          <input
+            id="numDoors"
+            value={this.props.doors}
+            onChange={this.changeDoors}
+          />
+          <label for="numDoors">Number of Doors</label>
+        </div>
+        <h2>Select your game mode</h2>
+        <div className="buttons">
+          <div
+            className="rowButton"
+            id="lightBlue"
+            onClick={this.handleClickStd}
+          >
+            <span>Standard Monty</span>
+          </div>
+          <div className="rowButton" id="red" onClick={this.handleClickHell}>
+            <span>Hell Monty</span>
+          </div>
+          <div
+            className="rowButton"
+            id="gold"
+            onClick={this.handleClickAngelic}
+          >
+            <span>Angelic Monty</span>
+          </div>
+          <div className="rowButton" id="violet" onClick={this.handleClickMind}>
+            <span>Mind-Reading Monty</span>
+          </div>
+          <div
+            className="rowButton"
+            id="skyBlue"
+            onClick={this.handleClickRandom}
+          >
+            <span>Random Monty</span>
+          </div>
+          <div
+            className="rowButton"
+            id="navy"
+            onClick={this.handleClickMystery}
+          >
+            <span>Mystery Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+          <div className="rowButton">
+            <span>??? Monty</span>
+          </div>
+        </div>
+        {options_button}
+        {this.state.options && (
+          <div>
+            <input
+              name="autoPlay"
+              type="checkbox"
+              checked={this.state.autoPlay}
+              onChange={(e) => this.setState({ autoPlay: e.target.checked })}
+            />
+            <span> Number of automatic games </span>
+            <input
+              value={this.state.auto_count}
+              onChange={(event) =>
+                this.setState({
+                  auto_count: event.target.value.replace(/\D/, ""),
+                })
+              }
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Sandbox;
