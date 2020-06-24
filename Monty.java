d0e75444582ccb83bf9ef56c818311744cfb93cd
guanminglin <60331909+guanminglin@users.noreply.github.com>
2020-06-24 02:11:16 -0400

    Update Monty.java

--- Monty.java
+++ Monty.java
@@ -70,6 +70,7 @@ abstract class Monty {
 
 		if (check_win()) { System.out.println("WIN"); }
 		else { System.out.println("LOSE");}
+		System.out.println("Prize was behind the door " + prize);
 	}
 
 	abstract public int open_door();
@@ -129,29 +130,117 @@ class AngelicMonty extends Monty {
 }
 
 class SandboxMonty extends Monty {
-	private double probability;
-	SandboxMonty(int n_doors, int chosen_door, int prize, double probability) {
+	private double[][] probability_matrix;
+	SandboxMonty(int n_doors, int chosen_door, int prize) {
 		super(n_doors, chosen_door, prize);
-		this.probability = probability;
+		probability_matrix = new double[n_doors][n_doors];
+	}
+
+	public void set_prize(int prize) {
+		this.prize = prize;
+	}
+
+	public void load_matrix() {
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0 ; i < n_doors; ++i) {
+			double check_sum = 0;
+			for (int j = 0; j < n_doors; ++j) {
+				double temp = scan.nextDouble();
+
+				assert temp <= 1;
+				assert temp >= 0;
+				if (j == prize - 1) assert temp == 0;
+
+				probability_matrix[i][j] = temp;
+				check_sum += temp;
+			}
+			assert check_sum == 1;
+		}
+		parse_matrix();
+	}
+
+	public void parse_matrix() {
+		for (int i = 0; i < n_doors; ++i) {
+			double running_sum = 0;
+			for (int j = 0; j < n_doors; ++j) {
+				running_sum += probability_matrix[i][j];
+				probability_matrix[i][j] = running_sum;
+			}
+		}
+	}
+
+	public void display_matrix() {
+		for (int i = 0; i < n_doors; ++i) {
+			for (int j = 0; j < n_doors; ++j) {
+				System.out.print(probability_matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 
 	public int open_door() {
 		Random rand = new Random();
-		// Open leftmost door with a probability p
-		if (rand.nextDouble() < this.probability) {
-			for (int i = 1; i <= n_doors; ++i) {
-				if (i != prize && i != chosen_door)
-					return i;
+		double temp = rand.nextDouble();
+
+		System.out.println("temp = " + temp);
+
+		for (int i = 1; i < n_doors; ++i) {
+			if (i == prize - 1) {
+				continue;
 			}
-		}
-		// Open right most door with a prbability 1-p
-		else {
-			for (int i = n_doors; i > 0; --i) {
-				if (i != prize && i != chosen_door)
-					return i;
+			else if (temp < probability_matrix[chosen_door][i]) {
+				return i + 1;
 			}
 		}
 		return 0;
+	}
+
+	public void execute() {
+		System.out.println("SANDBOX MODE");
+		System.out.print("Set prize behind door #");
+		Scanner scan = new Scanner(System.in);
+		set_prize(scan.nextInt());
+		System.out.println();
+		System.out.println("Enter an N x N probability matrix:");
+		load_matrix();
+
+		int open_door = open_door();
+		if (open_door != 0) {
+			System.out.println("Monty opens the door " + open_door);
+			if(check_door(open_door)) {
+				System.out.println(open_door + " is a car.");
+				System.out.println("LOSE");
+				return;
+			}
+			else {
+				System.out.println(open_door + " is not a car.");
+			}
+			System.out.print("Monty offers you to switch a door. Do you want to switch? Y/N: ");
+
+			String t = scan.next();
+			if (t.equalsIgnoreCase("Y")) {
+				System.out.print("What door do you want to switch to? Door = ");
+
+				int temp = scan.nextInt();
+				assert temp <= n_doors;
+				if(temp < 1 || temp > n_doors)
+				{
+					System.out.println("Your have chosen an invalid door");
+					//do something else here!
+					System.exit(1);
+				}
+				if(temp == chosen_door)
+				{
+					System.out.println("You cannot switch to the same door");
+					System.exit(1);
+				}
+				switch_door(temp);
+			}
+		}
+
+		if (check_win()) { System.out.println("WIN"); }
+		else { System.out.println("LOSE");}
+		System.out.println("Prize was behind the door " + prize);
 	}
 }
 
