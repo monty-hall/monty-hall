a8868fe2296e48f8dc02a6fb644e97888b2c906c
golcz <49885863+golcz@users.noreply.github.com>
2020-08-05 13:06:29 -0400

    Add files via upload
    Extended testAll.java file. Now it tests all the different standard Monty types.

--- TestAll.java
+++ TestAll.java
@@ -61,6 +61,41 @@ public final class TestAll {
     	double t = ((double)delta/tries)/(Math.sqrt((delta_sq-(double)delta*delta/tries)/(tries*(tries-1))));
     	return t;
      }
+     public double compareStatOne(int tries,int[] results)
+     {	
+    	int delta = 0;
+ 		int delta_sq = 0;
+ 		
+    	for(int i=0; i<tries; ++i)
+ 	    {
+ 		    int ranVarBool = 1;
+ 		    	
+ 		    delta+=results[i]-ranVarBool;
+ 		    delta_sq+=(results[i]-ranVarBool)*(results[i]-ranVarBool);
+ 		    
+ 	    }
+    	double t = ((double)delta/tries)/(Math.sqrt((delta_sq-(double)delta*delta/tries)/(tries*(tries-1))));
+    	return t;
+     }
+     public double compareStatZero(int tries,int[] results)
+     {	
+    	int delta = 0;
+ 		int delta_sq = 0;
+ 		
+    	for(int i=0; i<tries; ++i)
+ 	    {
+
+ 		    int ranVarBool = 0;
+ 		    	
+ 		    delta+=results[i]-ranVarBool;
+ 		    delta_sq+=(results[i]-ranVarBool)*(results[i]-ranVarBool);
+ 		    
+ 	    }
+    	double t = ((double)delta/tries)/(Math.sqrt((delta_sq-(double)delta*delta/tries)/(tries*(tries-1))));
+    	if(Double.isNaN(t))
+    		return 0;
+    	return t;
+     }
 
 	@Test
 	 public void testMontyConstructor() {
@@ -68,7 +103,11 @@ public final class TestAll {
 		Random rand = new Random(System.currentTimeMillis());
 	    int prize = rand.nextInt(n_doors) + 1;
 	    
+	    //standard Monty's
 	    new StandardMonty(n_doors, choice_door, prize);
+	    new InYourFaceStandardMonty(n_doors, choice_door, prize);
+	    new TooBadStandardMonty(n_doors,choice_door,prize);
+	    new TrickyStandardMonty(n_doors,choice_door,prize);
 	  }
 	@Test
 	public void testStandardMontyStay(){
@@ -85,6 +124,7 @@ public final class TestAll {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	    System.out.println("testStandardMontyStay:");
 	    
 	    Random rand = new Random(System.currentTimeMillis());
 	    
@@ -128,6 +168,7 @@ public final class TestAll {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	    System.out.println("testStandardMontySwitch:");
 	    
 	    Random rand = new Random(System.currentTimeMillis());
 	    
@@ -166,6 +207,262 @@ public final class TestAll {
 	    System.out.println("t-value is " + t);
 	    assert(Math.abs(t)<1.282);
 	}
+	@Test
+	public void testInYourFaceStandardMontyStay(){
+		
+		PrintStream stdout = System.out;
+		int tries=100000;
+		int successTries=0;
+		
+		int[] results;
+		results = new int[tries];
+	     
+	    try {
+			System.setOut(new PrintStream(new File("testFile.txt")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("testInYourFaceStandardMontyStay:");
+	    
+	    Random rand = new Random(System.currentTimeMillis());
+	    
+	    for(int i=0; i<tries; ++i)
+	    {
+		    int prize = rand.nextInt(n_doors) + 1;
+		    int chosen_door = rand.nextInt(n_doors) + 1;
+		    	
+		    m = new InYourFaceStandardMonty(n_doors, chosen_door, prize);
+		    String data = Integer.toString(chosen_door);
+		    System.setIn(new ByteArrayInputStream(data.getBytes()));
+		    int ans = m.execute()? 1 : 0;
+		    results[i]=ans;
+		    
+		    if(ans == 1)
+		    	successTries++;
+		    
+	    }
+	    
+	    double t = compareStatOneThird(tries,results);
 
 	    
+	    System.setOut(stdout);
+	    System.out.println("testInYourFaceStandardMontyStay: "+(double)successTries/tries);
+	    System.out.println("t-value is " + t);
+	    assert(Math.abs(t)<1.282);
+	}
+	@Test
+	public void testInYourFaceStandardMontySwitch(){
+		
+		PrintStream stdout = System.out;
+		int tries=100000;
+		int successTries=0;
+		
+		int[] results;
+		results = new int[tries];
+	    
+		
+	    try {
+			System.setOut(new PrintStream(new File("testFile.txt")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("testInYourFaceStandardMontySwitch:");
+	    
+	    Random rand = new Random(System.currentTimeMillis());
+	    
+	    for(int i=0; i<tries; ++i)
+	    {
+		    int prize = rand.nextInt(n_doors) + 1;
+		    int chosen_door = rand.nextInt(n_doors) + 1;
+		    
+		    
+		    int new_door = rand.nextInt(n_doors) + 1;
+		    while(new_door == chosen_door)
+		    {
+		    	new_door = rand.nextInt(n_doors) + 1;
+		    }
+		    int new_new_door = rand.nextInt(n_doors)+1;
+		    while(new_new_door == chosen_door || new_new_door == new_door)
+		    {
+		    	new_new_door = rand.nextInt(n_doors) + 1;
+		    }
+		    String data = Integer.toString(new_door)+"\n"+Integer.toString(new_new_door);
+		    System.setIn(new ByteArrayInputStream(data.getBytes()));
+		    	
+		    m = new InYourFaceStandardMonty(n_doors, chosen_door, prize);
+		    int ans = m.execute() ? 1 : 0;
+		    results[i]=ans;
+		    
+		    if(ans == 1)
+		    	successTries++;
+		    
+	    }
+	    
+	    double t = compareStatZero(tries,results);
+	    
+	    System.setOut(stdout);
+	    System.out.println("testInYourFaceStandardMontySwitch: "+(double)successTries/tries);
+	    System.out.println("t-value is " + t);
+	    assert(Math.abs(t)<1.282);
+	}
+	
+	//there is no public void testTooBadStandardMontyStay()
+	//because Monty will always open this door
+
+	@Test
+	public void testTooBadStandardMontySwitch(){
+		
+		PrintStream stdout = System.out;
+		int tries=100000;
+		int successTries=0;
+		
+		int[] results;
+		results = new int[tries];
+	    
+		
+	    try {
+			System.setOut(new PrintStream(new File("testFile.txt")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    System.out.println("testTooBadStandardMontySwitch:");
+	    
+	    Random rand = new Random(System.currentTimeMillis());
+	    
+	    for(int i=0; i<tries; ++i)
+	    {
+		    int prize = rand.nextInt(n_doors) + 1;
+		    int chosen_door = rand.nextInt(n_doors) + 1;
+		    
+		    
+		    int new_door = rand.nextInt(n_doors) + 1;
+		    while(new_door == chosen_door)
+		    {
+		    	new_door = rand.nextInt(n_doors) + 1;
+		    }
+		    int new_new_door = rand.nextInt(n_doors)+1;
+		    while(new_new_door == chosen_door || new_new_door == new_door)
+		    {
+		    	new_new_door = rand.nextInt(n_doors) + 1;
+		    }
+		    String data = Integer.toString(new_door)+"\n"+Integer.toString(new_new_door);
+		    System.setIn(new ByteArrayInputStream(data.getBytes()));
+		    	
+		    m = new TooBadStandardMonty(n_doors, chosen_door, prize);
+		    int ans = m.execute() ? 1 : 0;
+		    results[i]=ans;
+		    
+		    if(ans == 1)
+		    	successTries++;
+		    
+	    }
+	    
+	    double t = compareStatOneThird(tries,results);
+	    
+	    System.setOut(stdout);
+	    System.out.println("testTooBadStandardMontySwitch: "+(double)successTries/tries);
+	    System.out.println("t-value is " + t);
+	    assert(Math.abs(t)<1.282);
+	}
+
+
+@Test
+public void testTrickyStandardMontyStay(){
+	
+	PrintStream stdout = System.out;
+	int tries=100000;
+	int successTries=0;
+	
+	int[] results;
+	results = new int[tries];
+     
+    try {
+		System.setOut(new PrintStream(new File("testFile.txt")));
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+    System.out.println("testTrickyStandardMontyStay:");
+    
+    Random rand = new Random(System.currentTimeMillis());
+    
+    for(int i=0; i<tries; ++i)
+    {
+	    int prize = rand.nextInt(n_doors) + 1;
+	    int chosen_door = rand.nextInt(n_doors) + 1;
+	    	
+	    m = new TrickyStandardMonty(n_doors, chosen_door, prize);
+	    String data = Integer.toString(chosen_door);
+	    System.setIn(new ByteArrayInputStream(data.getBytes()));
+	    int ans = m.execute()? 1 : 0;
+	    results[i]=ans;
+	    
+	    if(ans == 1)
+	    	successTries++;
+	    
+    }
+    
+    double t = compareStatOneThird(tries,results);
+
+    
+    System.setOut(stdout);
+    System.out.println("testTrickyStandardMontyStay: "+(double)successTries/tries);
+    System.out.println("t-value is " + t);
+    assert(Math.abs(t)<1.282);
+}
+@Test
+public void testTrickyStandardMontySwitch(){
+	
+	PrintStream stdout = System.out;
+	int tries=100000;
+	int successTries=0;
+	
+	int[] results;
+	results = new int[tries];
+    
+	
+    try {
+		System.setOut(new PrintStream(new File("testFile.txt")));
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+    System.out.println("testTrickyStandardMontySwitch:");
+    
+    Random rand = new Random(System.currentTimeMillis());
+    
+    for(int i=0; i<tries; ++i)
+    {
+	    int prize = rand.nextInt(n_doors) + 1;
+	    int chosen_door = rand.nextInt(n_doors) + 1;
+	    
+	    
+	    int new_door = rand.nextInt(n_doors) + 1;
+	    while(new_door == chosen_door)
+	    {
+	    	new_door = rand.nextInt(n_doors) + 1;
+	    }
+	    int new_new_door = rand.nextInt(n_doors)+1;
+	    while(new_new_door == chosen_door || new_new_door == new_door)
+	    {
+	    	new_new_door = rand.nextInt(n_doors) + 1;
+	    }
+	    String data = Integer.toString(new_door)+"\n"+Integer.toString(new_new_door);
+	    System.setIn(new ByteArrayInputStream(data.getBytes()));
+	    	
+	    m = new TrickyStandardMonty(n_doors, chosen_door, prize);
+	    int ans = m.execute() ? 1 : 0;
+	    results[i]=ans;
+	    
+	    if(ans == 1)
+	    	successTries++;
+	    
+    }
+    
+    double t = compareStatZero(tries,results);
+    
+    System.setOut(stdout);
+    System.out.println("testTrickyStandardMontySwitch: "+(double)successTries/tries);
+    System.out.println("t-value is " + t);
+    assert(Math.abs(t)<1.282);
+}
+
 }
